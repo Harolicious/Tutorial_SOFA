70cf6f2b3c4ce8b9d90a2b488dd4f16cf715c522
Harold Valenzuela-Coloma <96555835+Harolicious@users.noreply.github.com>
2025-08-19 19:00:57 -0400

    ready

--- modelo_SPC/SPC_model.py
+++ modelo_SPC/SPC_model.py
@@ -46,8 +46,10 @@ def createScene(rootNode):
                 )
                 # rootNode.addObject('VisualStyle', displayFlags='showVisualModels hideBehaviorModels showCollisionModels hideBoundingCollisionModels showForceFields showInteractionForceFields hideWireframe')
 
+                rootNode.addObject('RequiredPlugin', name='Sofa.Component.Topology.Mapping') # Needed to use components [Tetra2TriangleTopologicalMapping]
                 rootNode.addObject('FreeMotionAnimationLoop')
                 rootNode.addObject('GenericConstraintSolver', maxIterations=100, tolerance = 0.0000001)
+                rootNode.dt = 0.2
 
 		#cubito
                 cubito = rootNode.addChild('cubito')
@@ -57,55 +59,38 @@ def createScene(rootNode):
 
                 cubito.addObject('ShewchukPCGLinearSolver', iterations=15, name='linearsolver', tolerance=1e-5, preconditioners='preconditioner', use_precond=True, update_step=1)
 
-                Loader = cubito.addObject('MeshVTKLoader', name='loader', filename='CubitoEstirar.vtk')
-                Container = cubito.addObject('TetrahedronSetTopologyContainer', src='@loader', name='container')
+                cubito.addObject('MeshVTKLoader', name='loader', filename='A.vtk')
+                cubito.addObject('TetrahedronSetTopologyContainer', src='@loader', name='container')
                 cubito.addObject('TetrahedronSetTopologyModifier')
 
-                MO = cubito.addObject('MechanicalObject', name='tetras', template='Vec3', showIndices=False)
+                cubito.addObject('MechanicalObject', name='tetras', template='Vec3', showIndices=False)
                 cubito.addObject('UniformMass', totalMass=0.5)
                 
-                boxROIStiffness = cubito.addObject('BoxROI', name='boxROIStiffness', box=[-15, 21, -15,  15, 24, 15], drawBoxes=True, position="@tetras.rest_position", tetrahedra="@container.tetrahedra")
-                Container.init()
-                MO.init()
-                boxROIStiffness.init()
-                YM1 = 180000
-                YM2 = YM1*100
-                YMArray = np.ones(len(Loader.tetras))*YM1
-                IdxElementsInROI = np.array(boxROIStiffness.tetrahedronIndices.value)
-                YMArray[IdxElementsInROI] = YM2
-                print(f"len IdxElementsInROI: {len(IdxElementsInROI)}")
-                
-                print(f"Largo de YMArray:{len(YMArray)}")
-                cubito.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.3,  youngModulus=YMArray.flatten().tolist())
+    
+                cubito.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.4,  youngModulus=1000000)
 
-                cubito.addObject('BoxROI', name='boxROI', box=[-15, -1, -15,  15, 2, 15], drawBoxes=True, position="@tetras.rest_position", tetrahedra="@container.tetrahedra")
+                cubito.addObject('BoxROI', name='boxROI', box=[20, 20, 50,  -20, -20, 35], drawBoxes=True, position="@tetras.rest_position", tetrahedra="@container.tetrahedra")
                 cubito.addObject('RestShapeSpringsForceField', points='@boxROI.indices', stiffness=1e12)
-                cubito.addObject('GenericConstraintCorrection', solverName='preconditioner')
+                cubito.addObject('GenericConstraintCorrection', linearSolver='@preconditioner')
 
-                
-
-                
+                              
                 
 		#cubito/cavity
                 
         
                 cavity = cubito.addChild('cavity')
-                cavity.addObject('MeshSTLLoader', name='loader', filename='CubitoEstirar_Cavity.stl')
+                cavity.addObject('MeshSTLLoader', name='loader', filename='B.stl')
                 cavity.addObject('MeshTopology', src='@loader', name='topo')
                 cavity.addObject('MechanicalObject', name='cavity')
-                cavity.addObject('SurfacePressureConstraint', triangles='@topo.triangles', value=1000, valueType=0)
-                #cavity.addObject('BarycentricMapping', name='mapping',  mapForces=True, mapMasses=False)
+                cavity.addObject('SurfacePressureConstraint', triangles='@topo.triangles', value=-1500, valueType=0)
                 cavity.addObject('BarycentricMapping', name='mapping',  mapForces=True, mapMasses=True)
 
 
 		#cubito/cubitoVisu
                 cubitoVisu = cubito.addChild('visu')
-                cubitoVisu.addObject('TriangleSetTopologyContainer', name='container')
-                cubitoVisu.addObject('TriangleSetTopologyModifier')
-                cubitoVisu.addObject('Tetra2TriangleTopologicalMapping', name='Mapping', input="@../container", output="@container")
-
-                cubitoVisu.addObject('OglModel', color=[0.3, 0.2, 0.2, 0.6])
-                cubitoVisu.addObject('IdentityMapping')
+                cubitoVisu.addObject("MeshSTLLoader", filename="A_visu.stl", name="loader")
+                cubitoVisu.addObject("OglModel", src="@loader")
+                cubitoVisu.addObject("BarycentricMapping")
 
 
                 return rootNode
--- modelo_SPC/generateMeshes.py
+++ modelo_SPC/generateMeshes.py
@@ -3,20 +3,27 @@ import gmsh
 
 gmsh.initialize()
 
-gmsh.merge("Parte_A.STEP")
+gmsh.merge("A.STEP")
 
 gmsh.model.mesh.generate(3)
 gmsh.model.mesh.refine()
-gmsh.write("PARTE_A.vtk")
+gmsh.write("A.vtk")
 gmsh.fltk.run()
-
 gmsh.clear()
 
-gmsh.merge("Parte_B.step")
+gmsh.merge("B.step")
 
 gmsh.model.mesh.generate(2)
 gmsh.model.mesh.refine()
-
-# gmsh.model.mesh.refine()
-gmsh.write("PARTE_B.stl")
+gmsh.write("B.stl")
 gmsh.fltk.run()
+gmsh.clear()
+
+gmsh.merge("A.STEP")
+
+gmsh.model.mesh.generate(2)
+gmsh.model.mesh.refine()
+gmsh.model.mesh.refine()
+gmsh.write("A_visu.stl")
+gmsh.fltk.run()
+gmsh.clear()
